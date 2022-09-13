8a67427a663cbb1026730d1f86347b23ee61c05a
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-09-13 10:33:17 -0700

    Add passing tests

--- mythical-creatures/lib/vampire.rb
+++ mythical-creatures/lib/vampire.rb
@@ -1,5 +1,11 @@
 class Vampire
-  def initialize(name)
+  attr_reader :name, :pet, :thirsty
+  def initialize(name, pet = "bat")
     @name = name
+    @pet = pet
+    @thirsty = true
+  end
+  def drink
+    @thirsty = false
   end
 end
